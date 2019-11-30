c797cb57def4fdd685c98237f495e06a40fa154b
Yuval Allweil <themarquisdesheric@gmail.com>
2019-11-30 13:56:26 -0800

    this is a test to see if it truncates the commit m

--- amber-scripts.bash
+++ amber-scripts.bash
@@ -1,16 +1,48 @@
 #!/bin/bash
 
+IMAGE_DIRECTORY="$HOME/Desktop/Code/amber-health/public/img"
+ARTICLE_DIRECTORY="$HOME/Desktop/Code/amber-health/src/pages/articles"
+
+# aliases
+
+alias images="open $IMAGE_DIRECTORY"
+alias articles="code $ARTICLE_DIRECTORY"
+alias preview="npm i && npm run develop"
+alias update="git pull && npm i"
+
+#functions
+
+# commits, pulls, and pushes to GitHub
+
+function publish() {
+  echo -e "Please provide a short commit message explaining your changes (not more than 50 characters: you can test this in VScode's Source Control tab)\n"
+
+  read COMMIT_MSG
+
+  if [[ ${#COMMIT_MSG} -gt 50 ]]; then
+    TRUNCATED_COMMIT_MSG=${COMMIT_MSG:0:50}
+
+    echo -e "\n Message over 50 characters, truncated to:\n $TRUNCATED_COMMIT_MSG \n"
+
+    git commit -m "$TRUNCATED_COMMIT_MSG"
+  else
+    git commit -m "$COMMIT_MSG"
+  fi
+
+  git pull && git push
+}
+
 # templates a new markdown article for The Chronic site
 
 function createArticle() {
   CURRENT_DATE=`date +%Y-%m-%d`
 
   echo -e "\n\033[36m Please enter a title for your article: \033[00m\n"
-  read TITLE
+  read RAW_TITLE
 
-  KEBAB_CASE_TITLE=${TITLE// /-}
-  LOWER_KEBAB_CASE=$(echo $KEBAB_CASE_TITLE | tr '[:upper:]' '[:lower:]')
-  FILE_PATH="$HOME/Desktop/Code/amber-health/$CURRENT_DATE-$LOWER_KEBAB_CASE.md"
+  KEBAB_CASE_TITLE=${RAW_TITLE// /-}
+  TITLE=$(echo $KEBAB_CASE_TITLE | tr '[:upper:]' '[:lower:]')
+  FILE_PATH="$ARTICLE_DIRECTORY/$CURRENT_DATE-$TITLE.md"
 
   echo -e "--- \ntemplateKey: article" > $FILE_PATH
   echo "title: $TITLE" >> $FILE_PATH
@@ -24,14 +56,12 @@ function createArticle() {
   echo -e "\n\033[36m Please enter the image name (with extension):\n\n\033[32m Example: pom-2.jpg \033[00m\n"
   read FEATURED_IMAGE
 
-  # check if image exists. If not, open images directory
-
   IMAGE_DIRECTORY="$HOME/Desktop/Code/amber-health/public/img"
 
   if [ ! -e $IMAGE_DIRECTORY/$FEATURED_IMAGE ]; then
     echo -e "\n\033[31m image \"$FEATURED_IMAGE\" not found. Opening images folder so you can add it now. Please make sure to give it the same name. \033[00m\n"
     
-    open $IMAGE_DIRECTORY
+    images # open image directory
   fi
 
   echo -e "featuredimage: /img/$FEATURED_IMAGE\ntags:\n  - MAKE\n  - THESE\n  - TAGS\n--- \n" >> $FILE_PATH
