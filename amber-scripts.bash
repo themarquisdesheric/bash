f1c4f1af0f56ad9d84f0e221140243b0cb40a652
Yuval Allweil <themarquisdesheric@gmail.com>
2019-11-29 16:55:53 -0800

    check for existence of image first

--- amber-scripts.bash
+++ amber-scripts.bash
@@ -15,7 +15,6 @@ function createArticle() {
   echo -e "--- \ntemplateKey: article" > $FILE_PATH
   echo "title: $TITLE" >> $FILE_PATH
 
-
   echo "date: $CURRENT_DATE" >> $FILE_PATH
 
   echo -e "\nPlease enter a description:\n"
@@ -27,7 +26,24 @@ function createArticle() {
 
   # check if image exists. If not, open images directory
 
-  echo "featuredimage: /img/$FEATURED_IMAGE" >> $FILE_PATH
+  IMAGE_DIRECTORY="$HOME/Desktop/Code/amber-health/public/img"
+
+  if [ ! -e "$IMAGE_DIRECTORY/$FEATURED_IMAGE" ]; then
+    echo -e "image \"$FEATURED_IMAGE\" not found. Opening images folder so you can add it now. Please make sure to give it the same name.\n"
+    
+    open $IMAGE_DIRECTORY
+  fi
+
+  echo -e "Waiting for you to add $FEATURED_IMAGE. Once you have added it, type \"proceed\" to continue.\n"
+
+  read RESPONSE
+
+  if [[ $RESPONSE == "proceed" ]]; then
+    echo "featuredimage: /img/$FEATURED_IMAGE" >> $FILE_PATH
+  else
+    echo -e "You'll need to add your image to the markdown in the editor\n"
+    echo "featuredimage: /img/ADD IMAGE NAME AND EXTENSION HERE" >> $FILE_PATH
+  fi
 
   # make tags
 
