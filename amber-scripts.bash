0eedab8922076b7ca0010919b132823199800087
Yuval Allweil <themarquisdesheric@gmail.com>
2019-11-29 19:23:36 -0800

    fix script

--- amber-scripts.bash
+++ amber-scripts.bash
@@ -28,24 +28,22 @@ function createArticle() {
 
   IMAGE_DIRECTORY="$HOME/Desktop/Code/amber-health/public/img"
 
-  if [ ! -e "$IMAGE_DIRECTORY/$FEATURED_IMAGE" ]; then
-    echo -e "image \"$FEATURED_IMAGE\" not found. Opening images folder so you can add it now. Please make sure to give it the same name.\n"
+  if [ ! -e $IMAGE_DIRECTORY/$FEATURED_IMAGE ]; then
+    echo -e "\nimage \"$FEATURED_IMAGE\" not found. Opening images folder so you can add it now. Please make sure to give it the same name.\n"
     
     open $IMAGE_DIRECTORY
+  
+    echo -e "Waiting for you to add $FEATURED_IMAGE. Once you have added it, type \"proceed\" to continue.\n"
+
+    read RESPONSE
+
+    if [[ $RESPONSE != "proceed" ]]; then
+      echo -e "\nYou'll need to add your image to the markdown in the editor\n"
+      echo "featuredimage: /img/ADD IMAGE NAME AND EXTENSION HERE" >> $FILE_PATH
+    fi
   fi
 
-  echo -e "Waiting for you to add $FEATURED_IMAGE. Once you have added it, type \"proceed\" to continue.\n"
+  echo -e "featuredimage: /img/$FEATURED_IMAGE\ntags:\n  - MAKE\n  - THESE\n  - TAGS\n--- \n" >> $FILE_PATH
 
-  read RESPONSE
-
-  if [[ $RESPONSE == "proceed" ]]; then
-    echo "featuredimage: /img/$FEATURED_IMAGE" >> $FILE_PATH
-  else
-    echo -e "You'll need to add your image to the markdown in the editor\n"
-    echo "featuredimage: /img/ADD IMAGE NAME AND EXTENSION HERE" >> $FILE_PATH
-  fi
-
-  # make tags
-
-  echo -e "--- \n" >> $FILE_PATH
+  code $FILE_PATH
 }
